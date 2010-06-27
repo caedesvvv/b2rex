40393aa239a894b3c33cf6c9cdd02443ff82b4ed
Pablo Martin <caedes@grupoikusnet.com>
2010-06-27 18:42:31 +0200

    adding options to handle position offset.

--- scripts/b2rexpkg/settings.py
+++ scripts/b2rexpkg/settings.py
@@ -3,6 +3,8 @@ import os
 import Blender
 from Blender import Registry
 
+from ogredotscene import BoundedValueModel
+
 #from ogredotscene import BoundedValueModel
 class ExportSettings:
     """Global export settings.
@@ -17,19 +19,19 @@ class ExportSettings:
         self.fileName = ''
         self.fixUpAxis = 0
         self.doProperties = 0
-        self.rotX = BoundedValueModel(-1000.0, 1000.0, 0.0)
-        self.rotY = BoundedValueModel(-1000.0, 1000.0, 0.0)
-        self.rotZ = BoundedValueModel(-1000.0, 1000.0, 0.0)
         self.scale = BoundedValueModel(0.0, 1e6, 1.0);
         self.load()
         """
+        self.locX = BoundedValueModel(-10000.0, 10000.0, 128.0)
+        self.locY = BoundedValueModel(-10000.0, 10000.0, 128.0)
+        self.locZ = BoundedValueModel(-1000.0, 1000.0, 20.0)
         return
-    def getRotX(self):
-        return self.rotX.getValue()
-    def getRotY(self):
-        return self.rotY.getValue()
-    def getRotZ(self):
-        return self.rotZ.getValue()
+    def getLocX(self):
+        return self.locX.getValue()
+    def getLocY(self):
+        return self.locY.getValue()
+    def getLocZ(self):
+        return self.locZ.getValue()
     def getScale(self):
         return self.scale.getValue()
     def load(self):
@@ -45,23 +47,12 @@ class ExportSettings:
                 self.server_url = settingsDict['server_url']
             if settingsDict.has_key('export_dir'):
                 self.export_dir = settingsDict['export_dir']
-            a = """
-            if settingsDict.has_key('fileName'):
-                self.fileName = settingsDict['fileName']
-            if settingsDict.has_key('fixUpAxis'):
-                self.fixUpAxis = settingsDict['fixUpAxis']
-            if settingsDict.has_key('doProperties'):
-                self.doProperties = settingsDict['doProperties']
-# float(integer) for compatibility
-            if settingsDict.has_key('rotX'):
-                self.rotX.setValue(float(settingsDict['rotX']))
-            if settingsDict.has_key('rotY'):
-                self.rotY.setValue(float(settingsDict['rotY']))
-            if settingsDict.has_key('rotZ'):
-                self.rotZ.setValue(float(settingsDict['rotZ']))
-            if settingsDict.has_key('scale'):
-                self.scale.setValue(settingsDict['scale'])
-            """
+            if settingsDict.has_key('locX'):
+                self.locX.setValue(float(settingsDict['locX']))
+            if settingsDict.has_key('locY'):
+                self.locY.setValue(float(settingsDict['locY']))
+            if settingsDict.has_key('locZ'):
+                self.locZ.setValue(float(settingsDict['locZ']))
     def save(self):
         """Save settings to registry.
         """
@@ -70,6 +61,9 @@ class ExportSettings:
         settingsDict['pack'] = self.pack
         settingsDict['server_url'] = self.server_url
         settingsDict['export_dir'] = self.export_dir
+        settingsDict['locX'] = self.locX
+        settingsDict['locY'] = self.locY
+        settingsDict['locZ'] = self.locZ
         Registry.SetKey('b2rex', settingsDict, True) 
         return
 
