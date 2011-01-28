3c6ca026b376760b569ff57de1e658515031d669
Pablo Martin <caedes@sindominio.net>
2011-01-28 03:21:45 +0100

    comments.

--- scripts/b2rexpkg/passmanager.py
+++ scripts/b2rexpkg/passmanager.py
@@ -1,3 +1,7 @@
+"""
+Password Manager
+"""
+
 import user
 import os
 import json
@@ -10,26 +14,37 @@ class PasswordManager(object):
         if not os.path.exists(appdir):
             os.makedirs(appdir)
         self.appfile = os.path.join(appdir, "config")
-        print "CONFIG:",self.appfile
         if os.path.exists(self.appfile):
             self.load_config()
     def load_config(self):
+        """
+        Load the configuration.
+        """
         f = open(self.appfile, "r")
         data = f.read()
         f.close()
         self.config = json.loads(data)
 
     def save_config(self):
+        """
+        Save the configuration.
+        """
         if self.config:
             f = open(self.appfile, "w")
             data = f.write(json.dumps(self.config))
             f.close()
 
     def get_credentials(self, url):
+        """
+        Get the credentials for a certain server.
+        """
         if url in self.config:
             return self.config[url]
         return ("", "")
     def set_credentials(self, url, username, password):
+        """
+        Set the credentials for a certain server.
+        """
         self.config[url] = (username, password)
         self.save_config()
 
